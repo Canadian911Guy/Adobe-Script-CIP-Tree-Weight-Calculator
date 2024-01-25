fe5dce6e7b2e8285049c7e2bad14da3884e903d4
Canadian911Guy <mark@markcoderre.ca>
2024-01-25 09:45:34 -0800

    Various Updates

--- Conversions-Reset-Hide.js
+++ Conversions-Reset-Hide.js
@@ -76,139 +76,33 @@ function updateLargeDiameterFields(doc, value, unit) {
 }
 
 function resetForm() {
+    // List default values for dropdowns
+    var defaultDropdownValues = {
+        "Species": "Choose Tree Species",
+        "Species-2": "Choose Tree Species" // Add more as needed
+    };
+
     var numFields = this.numFields;
     for (var i = 0; i < numFields; i++) {
         var fieldName = this.getNthFieldName(i);
         var field = this.getField(fieldName);
-        if (field.type !== "button") { // Skip buttons
-            field.value = ""; // Reset field value
+
+        if (field.type === "combobox" || field.type === "listbox") {
+            field.value = defaultDropdownValues[fieldName] || ""; // Reset to default value or empty string
+        } else if (field.type !== "button") {
+            field.value = ""; // Clear other field types
+        }
+    }
+
+    // List of text and image fields to hide
+    var fieldsToHide = ["Text5", "Image5_af_image", "Image6_af_image", "Text7", "Image8_af_image", "Image9_af_image", "Text1", "Image3_af_image", "Image4_af_image", "Text6", "Image7_af_image", "Image10_af_image", "Text11", "Image12_af_image", "Image13_af_image", "Text14", "Image15_af_image", "Image16_af_image", "Text17", "Image18_af_image", "Image19_af_image", "Text20", "Image21_af_image", "Image22_af_image", "Text23", "Image24_af_image", "Image25_af_image", "Text26", "Image27_af_image", "Image28_af_image", "Text29", "Image30_af_image", "Image31_af_image", "Text32", "Image33_af_image", "Image34_af_image", "Text35", "Image36_af_image", "Image37_af_image", "Text38", "Image39_af_image", "Image40_af_image", "Text4", "Image1_af_image", "Image2_af_image", "Text41", "Image42_af_image", "Image43_af_image", "Text44", "Image45_af_image", "Image46_af_image", "Text47", "Image48_af_image", "Image49_af_image", "Text50", "Image51_af_image", "Image52_af_image", "Text53", "Image54_af_image", "Image55_af_image", "Text56", "Image57_af_image", "Image58_af_image", "Text59", "Image60_af_image", "Image61_af_image", "Text62", "Image63_af_image", "Image64_af_image", "Text65", "Image66_af_image", "Image67_af_image", "Text68", "Image69_af_image", "Image70_af_image", "Text71", "Image72_af_image", "Image73_af_image", "Text2", "Image11_af_image", "Image14_af_image", "Text15", "Image17_af_image", "Image20_af_image", "Text21", "Image23_af_image", "Image26_af_image", "Text27", "Image29_af_image", "Image32_af_image", "Text33", "Image35_af_image", "Image38_af_image", "Text39", "Image41_af_image", "Image44_af_image", "Text45", "Image47_af_image", "Image50_af_image", "Text51", "Image53_af_image", "Image56_af_image", "Text57", "Image59_af_image", "Image62_af_image", "Text63", "Image65_af_image", "Image68_af_image", "Text69", "Image71_af_image", "Image74_af_image", "Text75", "Image76_af_image", "Image77_af_image", "Text78", "Image79_af_image", "Image80_af_image", "Text79", "Image81_af_image", "Image82_af_image", "Text3", "Image75_af_image", "Image78_af_image", "Text8", "Image83_af_image", "Image84_af_image"];
+
+    // Loop through each field and set its display to hidden
+    for (var j = 0; j < fieldsToHide.length; j++) {
+        var hideFieldName = fieldsToHide[j];
+        var hideField = this.getField(hideFieldName);
+        if (hideField !== null) {
+            hideField.display = display.hidden;
         }
     }
 }
-
-// Set Text and Images to hidden
-			this.getField("Text5").display = display.hidden;
-            this.getField("Image5_af_image").display = display.hidden;
-            this.getField("Image6_af_image").display = display.hidden;
-            this.getField("Text7").display = display.hidden;
-            this.getField("Image8_af_image").display = display.hidden;
-            this.getField("Image9_af_image").display = display.hidden;
-            this.getField("Text1").display = display.hidden;
-            this.getField("Image3_af_image").display = display.hidden;
-            this.getField("Image4_af_image").display = display.hidden;
-            this.getField("Text6").display = display.hidden;
-            this.getField("Image7_af_image").display = display.hidden;
-            this.getField("Image10_af_image").display = display.hidden;
-            this.getField("Text11").display = display.hidden;
-            this.getField("Image12_af_image").display = display.hidden;
-            this.getField("Image13_af_image").display = display.hidden;
-            this.getField("Text14").display = display.hidden;
-            this.getField("Image15_af_image").display = display.hidden;
-            this.getField("Image16_af_image").display = display.hidden;
-            this.getField("Text17").display = display.hidden;
-            this.getField("Image18_af_image").display = display.hidden;
-            this.getField("Image19_af_image").display = display.hidden;
-            this.getField("Text20").display = display.hidden;
-            this.getField("Image21_af_image").display = display.hidden;
-            this.getField("Image22_af_image").display = display.hidden;
-            this.getField("Text23").display = display.hidden;
-            this.getField("Image24_af_image").display = display.hidden;
-            this.getField("Image25_af_image").display = display.hidden;
-            this.getField("Text26").display = display.hidden;
-            this.getField("Image27_af_image").display = display.hidden;
-            this.getField("Image28_af_image").display = display.hidden;
-            this.getField("Text29").display = display.hidden;
-            this.getField("Image30_af_image").display = display.hidden;
-            this.getField("Image31_af_image").display = display.hidden;
-            this.getField("Text32").display = display.hidden;
-            this.getField("Image33_af_image").display = display.hidden;
-            this.getField("Image34_af_image").display = display.hidden;
-            this.getField("Text35").display = display.hidden;
-            this.getField("Image36_af_image").display = display.hidden;
-            this.getField("Image37_af_image").display = display.hidden;
-            this.getField("Text38").display = display.hidden;
-            this.getField("Image39_af_image").display = display.hidden;
-            this.getField("Image40_af_image").display = display.hidden;
-            this.getField("Text4").display = display.hidden;
-            this.getField("Image1_af_image").display = display.hidden;
-            this.getField("Image2_af_image").display = display.hidden;
-            this.getField("Text41").display = display.hidden;
-            this.getField("Image42_af_image").display = display.hidden;
-            this.getField("Image43_af_image").display = display.hidden;
-            this.getField("Text44").display = display.hidden;
-            this.getField("Image45_af_image").display = display.hidden;
-            this.getField("Image46_af_image").display = display.hidden;
-            this.getField("Text47").display = display.hidden;
-            this.getField("Image48_af_image").display = display.hidden;
-            this.getField("Image49_af_image").display = display.hidden;
-            this.getField("Text50").display = display.hidden;
-            this.getField("Image51_af_image").display = display.hidden;
-            this.getField("Image52_af_image").display = display.hidden;
-            this.getField("Text53").display = display.hidden;
-            this.getField("Image54_af_image").display = display.hidden;
-            this.getField("Image55_af_image").display = display.hidden;
-            this.getField("Text56").display = display.hidden;
-            this.getField("Image57_af_image").display = display.hidden;
-            this.getField("Image58_af_image").display = display.hidden;
-            this.getField("Text59").display = display.hidden;
-            this.getField("Image60_af_image").display = display.hidden;
-            this.getField("Image61_af_image").display = display.hidden;
-            this.getField("Text62").display = display.hidden;
-            this.getField("Image63_af_image").display = display.hidden;
-            this.getField("Image64_af_image").display = display.hidden;
-            this.getField("Text65").display = display.hidden;
-            this.getField("Image66_af_image").display = display.hidden;
-            this.getField("Image67_af_image").display = display.hidden;
-            this.getField("Text68").display = display.hidden;
-            this.getField("Image69_af_image").display = display.hidden;
-            this.getField("Image70_af_image").display = display.hidden;
-            this.getField("Text71").display = display.hidden;
-            this.getField("Image72_af_image").display = display.hidden;
-            this.getField("Image73_af_image").display = display.hidden;
-            this.getField("Text2").display = display.hidden;
-            this.getField("Image11_af_image").display = display.hidden;
-            this.getField("Image14_af_image").display = display.hidden;
-            this.getField("Text15").display = display.hidden;
-            this.getField("Image17_af_image").display = display.hidden;
-            this.getField("Image20_af_image").display = display.hidden;
-            this.getField("Text21").display = display.hidden;
-            this.getField("Image23_af_image").display = display.hidden;
-            this.getField("Image26_af_image").display = display.hidden;
-            this.getField("Text27").display = display.hidden;
-            this.getField("Image29_af_image").display = display.hidden;
-            this.getField("Image32_af_image").display = display.hidden;
-            this.getField("Text33").display = display.hidden;
-            this.getField("Image35_af_image").display = display.hidden;
-            this.getField("Image38_af_image").display = display.hidden;
-            this.getField("Text39").display = display.hidden;
-            this.getField("Image41_af_image").display = display.hidden;
-            this.getField("Image44_af_image").display = display.hidden;
-            this.getField("Text45").display = display.hidden;
-            this.getField("Image47_af_image").display = display.hidden;
-            this.getField("Text51").display = display.hidden;
-            this.getField("Image53_af_image").display = display.hidden;
-            this.getField("Image56_af_image").display = display.hidden;
-            this.getField("Text57").display = display.hidden;
-            this.getField("Image59_af_image").display = display.hidden;
-            this.getField("Image62_af_image").display = display.hidden;
-            this.getField("Text63").display = display.hidden;
-            this.getField("Image65_af_image").display = display.hidden;
-            this.getField("Image68_af_image").display = display.hidden;
-            this.getField("Text69").display = display.hidden;
-            this.getField("Image71_af_image").display = display.hidden;
-            this.getField("Image74_af_image").display = display.hidden;
-            this.getField("Text75").display = display.hidden;
-            this.getField("Image76_af_image").display = display.hidden;
-            this.getField("Image77_af_image").display = display.hidden;
-            this.getField("Text78").display = display.hidden;
-            this.getField("Image79_af_image").display = display.hidden;
-            this.getField("Image80_af_image").display = display.hidden;
-			this.getField("Text3").display = display.hidden;
-            this.getField("Image75_af_image").display = display.hidden;
-            this.getField("Image78_af_image").display = display.hidden;
-            this.getField("Text79").display = display.hidden;
-            this.getField("Image81_af_image").display = display.hidden;
-            this.getField("Image82_af_image").display = display.hidden;
-            this.getField("Text8").display = display.hidden;
-            this.getField("Image83_af_image").display = display.hidden;
-            this.getField("Image84_af_image").display = display.hidden;
